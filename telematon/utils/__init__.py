84fa7eb12f387e1f3ac09390cd6b5bc894070b6d
Pavlo Pohorieltsev <667strets@gmail.com>
2023-07-01 14:03:23 +0300

    add import

--- telematon/utils/__init__.py
+++ telematon/utils/__init__.py
@@ -1,5 +1,6 @@
 from .create_contact import create_contact
+from .error_message import ErrorMessage
 from .get_contact import get_contact
 from .send_message import send_message
 
-__all__ = ["create_contact", "send_message", "get_contact"]
+__all__ = ["create_contact", "send_message", "get_contact", "ErrorMessage"]
